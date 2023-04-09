e3cef1e11df96bc55b344a5bcf9903230539fe12
yelo <zhihuzeye@gmail.com>
2023-04-09 19:12:04 +0800

    fix: load commonjs gitly

--- packages/@yelo/create/src/utils/fetch.ts
+++ packages/@yelo/create/src/utils/fetch.ts
@@ -1,7 +1,9 @@
 import { resolve } from 'node:path'
-import gitly from 'gitly'
+import Gitly from 'gitly'
 import { rimraf } from 'rimraf'
 import { paths } from './paths.js'
+
+const gitly = (Gitly as any).default as typeof Gitly
 
 const GITHUB_REPO = 'imyelo/create'
 
